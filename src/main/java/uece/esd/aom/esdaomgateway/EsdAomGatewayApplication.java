c3e250b107bb91a9269a17491ab6e34fb00a06eb
André Costa <andrecostan@gmail.com>
2024-05-06 16:50:41 -0300

    First commit

---
+++ src/main/java/uece/esd/aom/esdaomgateway/EsdAomGatewayApplication.java
@@ -0,0 +1,13 @@
+package uece.esd.aom.esdaomgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsdAomGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsdAomGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uece/esd/aom/esdaomgateway/EsdAomGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package uece.esd.aom.esdaomgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsdAomGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
